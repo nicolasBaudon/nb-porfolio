7db8e4be399fed28010d239be212bf4f32681b53
Nicolas Baudon <nicobaudon01@gmail.com>
2021-04-05 09:21:47 -0300

    .

--- src/pages/works/works.js
+++ src/pages/works/works.js
@@ -24,7 +24,7 @@ const Works = () => {
     <section className={styles.container}>
       <Grid container spacing={12}>
         <Grid item lg={12} md={12} sm={12} xs={12}>
-          <h2>Works</h2>
+          <h2>Some Works</h2>
           <nav>
             <ul>
               {works.map((item, i) => (
